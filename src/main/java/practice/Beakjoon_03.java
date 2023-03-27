04c6b0b08b8050c87e428ebf062b7cd3f34e32c5
euna <chna8137s@gmail.com>
2023-03-27 20:17:56 +0900

    Quiz 0327

---
+++ src/main/java/practice/Beakjoon_03.java
@@ -0,0 +1,16 @@
+package practice;
+import java.util.Scanner;
+public class Beakjoon_03 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        a = scan.nextInt();
+        b = scan.nextInt();
+
+        System.out.println(a*b);
+    }
+}
+
---
+++ src/main/java/practice/Beakjoon_04.java
@@ -0,0 +1,15 @@
+package practice;
+import java.util.Scanner;
+public class Beakjoon_04 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        double a, b;
+
+        a = scan.nextDouble();
+        b = scan.nextDouble();
+
+        System.out.println(a/b);
+    }
+}
