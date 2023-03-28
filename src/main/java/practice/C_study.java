60405988d051b1dc28976eb9a4446d357c249a75
euna <chna8137s@gmail.com>
2023-03-29 00:41:00 +0900

    Quiz 0327

---
+++ src/main/java/practice/C_study.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.util.Scanner;
+
+public class C_study {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int year;
+
+        System.out.printf("년도를 입력하세요 : ");
+        year = sc.nextInt();
+
+        if(year%4==0&&year%100!=0){
+            System.out.println(year + "년은 윤년입니다.");}
+        else if(year%400==0){
+            System.out.println(year + "년은 윤년입니다.");
+        }
+        else{
+            System.out.println(year + "년은 윤년이 아닙니다.");}
+    }
+}
