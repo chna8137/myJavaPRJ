3853050817aada9d92ff259a507ca5067d02cea1
euna <chna8137s@gmail.com>
2023-03-29 00:07:06 +0900

    Quiz 0327

---
+++ src/main/java/practice/Pr.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Pr {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c, d;
+
+        System.out.printf("첫 번째 계산할 값을 입력하세요 => ");
+        a = sc.nextInt();
+
+        System.out.printf("두 번째 계산할 값을 입력하세요 => ");
+        b = sc.nextInt();
+
+        System.out.printf("세 번쨰 계산할 값을 입력하세요 => ");
+        c = sc.nextInt();
+
+        System.out.printf("네 번째 계산할 값을 입력하세요 => ");
+        d = sc.nextInt();
+
+        System.out.printf("%d + %d + %d + %d = %d", a, b, c, d,a+b+c+d);
+
+    }
+}
---
+++ src/main/java/practice/Pr_02.java
@@ -0,0 +1,40 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Pr_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a,b;
+        char c;
+
+        System.out.printf("첫 번째 수를 입력하세요 : ");
+        a = sc.nextInt();
+
+        System.out.printf("계산할 연산자를 입력하세요 : ");
+        c = sc.next().charAt(0);
+
+        System.out.printf("두 번째 수를 입력하세요 : ");
+        b = sc.nextInt();
+
+        if(c=='+'){
+            System.out.printf("%d + %d = %d \n", a, b, a+b);
+        }
+        else if(c=='-'){
+            System.out.printf("%d - %d = %d \n", a,b, a-b);
+        }
+        else if(c=='*'){
+            System.out.printf("%d * %d = %d \n", a,b, a*b);
+        }
+        else if(c=='/'){
+            System.out.printf("%d / %d = %d \n", a,b, a/b);
+        }
+        else if(c=='%'){
+            System.out.printf("%d %% %d = %d \n", a,b, a%b);
+        }
+        else{
+            System.out.printf("올바른 연산자가 아닙니다.");
+        }
+    }
+}
