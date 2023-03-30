dcf173240a357b8ff304532e6d06a976849e064f
data7 <chna8137s@gmail.com>
2023-03-30 21:55:24 +0900

    추가

---
+++ src/main/java/practice/Beakjoon_06.java
@@ -0,0 +1,15 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Beakjoon_06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String c;
+
+        c = sc.next();
+        System.out.printf("%s??!", c);
+
+    }
+}
---
+++ src/main/java/practice/Beakjoon_07.java
@@ -0,0 +1,15 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Beakjoon_07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+
+        a = sc.nextInt();
+        System.out.print(a - 543);
+    }
+
+}
