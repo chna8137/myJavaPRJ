525a65e25ebadc370b6a21d6f4a866041673436a
data7 <chna8137s@gmail.com>
2023-04-03 20:30:05 +0900

    23-04-03 백준 연습

---
+++ src/main/java/practice/Beakjoon_2884.java
@@ -0,0 +1,25 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Beakjoon_2884 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        if (m < 45) {
+            h--;
+            m = 60 - (45 - m);
+            if (h < 0) {
+                h = 23;
+            }
+            System.out.println(h + " " + m);
+        } else {
+            System.out.println(h + " " + (m - 45));
+        }
+
+    }
+
+}
