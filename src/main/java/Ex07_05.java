0dc7f65f78cca240bc1e948736dc0b7d6dcca3d8
data7 <chna8137s@gmail.com>
2023-04-07 17:19:21 +0900

    23-04-07

---
+++ src/main/java/Ex07_05.java
@@ -0,0 +1,12 @@
+public class Ex07_05 {
+    public static void main(String[] args) {
+        int a = 100;
+
+        while (a == 200) { //while은 조건이 맞을때만 작동
+            System.out.printf("while 문 내부에 들어 왓습니다.\n");
+        }
+        do {//do - while 문은 무조건 한번은 작동한다.
+            System.out.printf("do - while 문 내부에 들어 왔습니다.\n");
+        } while (a == 200);
+    }
+}
