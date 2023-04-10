cd2f89124946c22a79658d6bb0e2418681ac7ddb
data7 <chna8137s@gmail.com>
2023-04-10 20:48:08 +0900

    23-04-10

--- src/main/java/practice/Beakjonn_10871.java
+++ src/main/java/practice/Beakjonn_10871.java
@@ -6,19 +6,19 @@ public class Beakjonn_10871 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int[]arr = new int[a];
-        int x;
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int[] arr = new int[n];
 
-        for(int i = 0; i < arr.length; i++){
+
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-
-
-
-
-
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < x) {
+                System.out.print(arr[i] + " ");
+            }
+        }
 
     }
-
 }
