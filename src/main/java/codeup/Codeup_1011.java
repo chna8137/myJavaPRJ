347a453258405a565763fbf0a07510d822ae3c7b
chna8137 <chna8137s@gmail.com>
2023-04-12 14:08:25 +0900

    2023-04-12

---
+++ src/main/java/codeup/Codeup_1011.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup_1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char x;
+        x = sc.next().charAt(0);
+        System.out.println(x);
+    }
+}
