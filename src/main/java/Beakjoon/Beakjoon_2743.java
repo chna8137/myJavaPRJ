f6a004d2ccd136ee3e0660e25ae0f8150e36d65d
data7 <chna8137s@gmail.com>
2023-05-09 16:49:22 +0900

    23-05-09

---
+++ src/main/java/Beakjoon/Beakjoon_2743.java
@@ -0,0 +1,13 @@
+package Beakjoon;
+
+import java.util.Scanner;
+
+public class Beakjoon_2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int len = str.length();
+
+        System.out.println(len);
+    }
+}
---
+++ src/main/java/codeup/Codeup_1295.java
@@ -0,0 +1,28 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup_1295 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        char[] arr;
+        arr = str.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (65 <= arr[i] && arr[i] <= 90){
+                arr[i] = (char) (arr[i] + 32);
+                continue;
+            }
+            if (97 <= arr[i] && arr[i] <= 122) {
+                arr[i] = (char) (arr[i] - 32);
+                continue;
+            }
+        }
+
+        System.out.print(arr);
+    }
+}
+
+
