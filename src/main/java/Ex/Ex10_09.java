c466698fee45b969c389d142898e9faa2563fe87
data7 <chna8137s@gmail.com>
2023-05-12 16:19:41 +0900

    23-05-12

---
+++ src/main/java/Ex/Ex10_09.java
@@ -0,0 +1,17 @@
+package Ex;
+
+import java.io.FileInputStream;
+
+public class Ex10_09 {
+    public static void main(String[] args) throws Exception{
+        FileInputStream fis = new FileInputStream("text.txt");
+
+        int ch;
+
+        while((ch = fis.read()) != -1){
+            System.out.print((char) ch);
+
+        }
+        fis.close();
+    }
+}
