fee7658d06d40ad4fb60b4a68155582fb9830d07
chna8137 <chna8137s@gmail.com>
2023-05-29 13:30:26 +0900

    2023-05-29

---
+++ src/main/java/Programmers/P_181946.java
@@ -0,0 +1,15 @@
+package Programmers;
+
+import java.util.Scanner;
+
+public class P_181946 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        String result = a.replace(" ","");
+        System.out.println(a + b);
+    }
+}
---
+++ src/main/java/Programmers/P_181948.java
@@ -0,0 +1,8 @@
+package Programmers;
+
+public class P_181948 {
+    public static void main(String[] args) {
+
+        System.out.println("!@#$%^&*(\\\'\"<>?:;");
+    }
+}
