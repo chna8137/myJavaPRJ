90e4d1578a13d67ae803d0e6141c9052c46d5117
chna8137 <chna8137s@gmail.com>
2023-06-06 17:47:56 +0900

    2023-06-06

--- src/main/java/Programmers/P_181943.java
+++ src/main/java/Programmers/P_181943.java
@@ -1,9 +1,12 @@
 package Programmers;
 public class P_181943 {
     public String solution(String my_string, String overwrite_string, int s) {
+
         String answer = "";
+
         int len01 = overwrite_string.length();
         int len02 = my_string.length();
+
         String sub01 = my_string.substring(0, s);
         String sub02 = my_string.substring((s + len01), len02);
 
