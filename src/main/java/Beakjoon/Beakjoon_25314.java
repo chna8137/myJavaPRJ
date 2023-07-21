572a50b7dfd3b2b83bc55e9ad3d12698ff580b6d
chna8137 <chna8137s@gmail.com>
2023-07-21 17:30:16 +0900

    2023-07-21

---
+++ src/main/java/Beakjoon/Beakjoon_25314.java
@@ -0,0 +1,19 @@
+package Beakjoon;
+
+import java.util.Scanner;
+
+public class Beakjoon_25314 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String str = "";
+
+        for (int i = 0; i < n/4; i++) {
+            str += "long ";
+
+        }
+        System.out.println(str + "int");
+
+    }
+}
