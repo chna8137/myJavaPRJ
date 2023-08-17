d6fe6702d387bd9d9f0ffedf177da7da67515ea6
chna8137 <chna8137s@gmail.com>
2023-08-17 14:18:13 +0900

    2023-08-17 백준 문제 풀이

---
+++ src/main/java/Beakjoon/Beakjoon_2745.java
@@ -0,0 +1,29 @@
+package Beakjoon;
+
+import java.util.Scanner;
+
+public class Beakjoon_2745 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        int n = sc.nextInt();
+        int tmp = 1;
+        int ans = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+
+            if ('A' <= c && c <= 'Z') {
+                ans += (c - 'A' + 10) * tmp;
+            } else {
+                ans += (c - '0') * tmp;
+            }
+            tmp *= n;
+        }
+
+        System.out.println(ans);
+
+    }
+}
+
