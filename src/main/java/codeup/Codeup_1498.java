e4857e4df65ebe910a85dc99370b5fe2d31a9e7c
chna8137 <chna8137s@gmail.com>
2023-08-22 16:17:40 +0900

    2023-08-22 코드업 문제 풀이

---
+++ src/main/java/codeup/Codeup_1498.java
@@ -0,0 +1,24 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Codeup_1498 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int g = sc.nextInt();
+        int[] b = new int[101];
+        for (int i = 0; i < n; i++) {
+            b[i]=sc.nextInt();
+        }
+        for (int i = 0; i < n; i+=g) {
+            int min = b[i];
+            for (int j = i; j < i + g; j++) {//첫회전 i=0 j=0 j < 3 / 0,1,2
+                if (j < n) {
+                    if (min > b[j]) min = b[j];
+                }
+            }
+            System.out.print(min + " ");
+        }
+    }
+}
