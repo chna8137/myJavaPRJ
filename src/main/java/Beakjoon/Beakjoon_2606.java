ecddd5880349d9091eb78857d855be5b67096b2d
euna <chna8137s@gmail.com>
2024-01-16 15:25:29 +0900

    2024-01-16 백준 문제 풀이

---
+++ src/main/java/Beakjoon/Beakjoon_2606.java
@@ -0,0 +1,52 @@
+package Beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Beakjoon_2606 {
+    static int ComCnt, Ssang;
+    static int[][] arr;
+    static boolean[] visit;
+    static int cnt = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
+
+        ComCnt = Integer.parseInt(br.readLine());
+        Ssang = Integer.parseInt(br.readLine());
+
+        arr = new int[ComCnt + 1][ComCnt + 1];
+        visit = new boolean[ComCnt + 1];
+
+        for(int i = 1 ; i <= Ssang; i++) {
+
+            st = new StringTokenizer(br.readLine());
+
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            arr[num1][num2] = 1;
+            arr[num2][num1] = 1;
+        }
+
+        System.out.println(dfs(1) - 1);
+    }
+
+    static int dfs(int num) {
+
+        visit[num] = true;
+        cnt++;
+
+        for(int i = 1 ; i <= ComCnt; i++) {
+
+            if(arr[num][i] == 1 && !visit[i]) {
+
+                dfs(i);
+            }
+        }
+        return cnt;
+    }
+}
